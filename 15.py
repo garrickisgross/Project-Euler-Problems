57b1490fcbf9ab34a05751878a5a738ee0c49309
garrickisgross <ggross93@gmail.com>
2024-11-03 19:31:22 -0600

    solved (maybe kinda cheated)

---
+++ 15.py
@@ -0,0 +1,9 @@
+import math
+
+# permutations problem. 
+# https://math.stackexchange.com/questions/4541170/permutations-with-indistinguishable-objects-vs-distinguishable-objects-and-disti
+# 20 'rights' and 20 'downs' in the possibility matrix.
+
+print(math.factorial(40)/(math.factorial(20) * math.factorial(20)))
+
+
